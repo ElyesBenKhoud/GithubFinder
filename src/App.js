f1a360ce388e27defb6dfe7c44930cbf4d1a2e99
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2022-04-14 13:00:21 +0300

    routers added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Notfound from "./pages/Notfound";
+
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main>content</main>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<Notfound />} />
+            <Route path="/*" element={<Notfound />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
