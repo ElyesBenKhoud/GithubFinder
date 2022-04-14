b052b359b1cd50071fb9bb409e3ac57735b5876d
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2022-04-14 12:09:52 +0300

    create app and tailwindcss and daisyui setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-sky-400"> hello world</h1>
+    </div>
+  );
+}
+
+export default App;
