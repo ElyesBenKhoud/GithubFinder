ffd32742fc7c08013332da0e8741df927bebf572
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2022-04-14 12:49:24 +0300

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main>content</main>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
