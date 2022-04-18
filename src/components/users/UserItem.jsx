9d837e346c7c5050b8f9d551a32992de21430b65
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2022-04-18 12:40:51 +0300

    data passed to userItem component & errors fixed

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,5 +1,10 @@
-const UserItem = () => {
-  return <div>UserItem</div>;
+import PropTypes from "prop-types";
+
+const UserItem = ({ user }) => {
+  return <div>{user.login}</div>;
 };
 
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 export default UserItem;
