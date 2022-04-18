b2505a347dd6d7506b1f10717c94445c88a0f407
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2022-04-18 12:47:54 +0300

    avatar user displayed

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,7 +1,29 @@
 import PropTypes from "prop-types";
-
+import { Link } from "react-router-dom";
 const UserItem = ({ user: { login, avatar_url } }) => {
-  return <div>{login}</div>;
+  return (
+    <div className="card shadow-md compact side bg-base- 100">
+      <div className="flex-row items-center space-x-4 card-body">
+        <div>
+          <div className="avatar">
+            <div className="rounded-full shadow w-14 h-14">
+              <img src={avatar_url} alt="profile" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className="card-title">{login}</h2>
+          <Link
+            className="text-base-content text-opacity-40"
+            to={`/users/${login}`}
+          >
+            {" "}
+            Visit Profile
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 UserItem.propTypes = {
