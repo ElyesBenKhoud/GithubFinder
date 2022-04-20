c9ef8a600fed51262645df1314b6f586e5faf580
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2022-04-20 13:30:45 +0300

    AlertContext create and fixed

--- src/App.js
+++ src/App.js
@@ -6,24 +6,26 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Notfound from "./pages/Notfound";
 import { GithubProvider } from "./context/github/GithubContext";
-
+import AlertState from "./context/alert/AlertState";
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
-          <main className="container mx-auto px-3 pb-12">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/notfound" element={<Notfound />} />
-              <Route path="/*" element={<Notfound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+      <AlertState>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            <main className="container mx-auto px-3 pb-12">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/notfound" element={<Notfound />} />
+                <Route path="/*" element={<Notfound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AlertState>
     </GithubProvider>
   );
 }
